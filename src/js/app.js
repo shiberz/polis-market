60bbb399eaf0eaba6d9229a7799545285130c15d
Kirill Grishin <k.grishin@ips.su>
2018-12-13 14:42:44 +0300

    block toggler

--- src/js/app.js
+++ src/js/app.js
@@ -12,4 +12,15 @@ $(function(){
  		$loginIcon.toggleClass('icon--big', !isOpen);
  		$loginIcon.toggleClass('icon--small', isOpen);
  	});
+
+
+ 	$('.js-block-toggle').on('click', function(e) {
+ 		var $block = $(e.target).closest('.js-block');
+ 		var $icon = $(e.target).children('.js-block-icon');
+
+ 		var isOpen = !$block.hasClass('block--closed');
+ 		$block.toggleClass('block--closed', isOpen);
+ 		$icon.toggleClass('icon__up-arr', !isOpen);
+ 		$icon.toggleClass('icon__down-arr', isOpen);
+ 	});
 });
